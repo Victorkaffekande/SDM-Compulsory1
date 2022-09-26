209c7b7e040735f1fcdd3b076baa8d1145156ec9
magn20 <magnus.overgaard@live.dk>
2022-09-26 13:20:57 +0200

    Succesful created CreateReviewService test
    Co-Authored-By: Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>

--- ReviewTester/UnitTest1.cs
+++ ReviewTester/UnitTest1.cs
@@ -1,9 +1,39 @@
+using Moq;
+using SDM_Compulsory1.Interface;
+using SDM_Compulsory1.Service;
+
 namespace ReviewTester;
 
 public class UnitTest1
 {
+
     [Fact]
-    public void Test1()
+    public void CreateReviewService()
     {
+        //Arange
+        Mock<IReviewRepository> mockRepo = new Mock<IReviewRepository>();
+        //Act
+        ReviewService service = new ReviewService(mockRepo.Object);
+        //Assert
+        Assert.NotNull(service);
+        Assert.True(service is ReviewService);
     }
+    
+    
+    /**
+     * 
+     
+    [Fact]
+    public void GetNumberOfReviewsFromValidReviewer(int reviewer)
+    {
+        //Arrange
+        
+        
+        //Act
+        
+        
+        //Assert
+    }
+    
+    */
 }
---
+++ SDM-Compulsory1/Service/ReviewService.cs
@@ -0,0 +1,67 @@
+﻿using SDM_Compulsory1.Interface;
+
+namespace SDM_Compulsory1.Service;
+
+public class ReviewService
+{
+    public IReviewRepository _repo;
+    public ReviewService(IReviewRepository repository)
+    {
+        _repo = repository;
+    }
+
+    public int GetNumberOfReviewsFromReviewer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetAverageRateFromReviewer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfRatesByReviewer(int reviewer, int rate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfReviews(int movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetAverageRateOfMovie(int movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfRates(int movie, int rate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetMoviesWithHighestNumberOfTopRates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetMostProductiveReviewers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetTopRatedMovies(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetTopMoviesByReviewer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetReviewersByMovie(int movie)
+    {
+        throw new NotImplementedException();
+    }
+}
